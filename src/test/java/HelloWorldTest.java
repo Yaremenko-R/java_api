52fc0864f297a8532d7ab886a537726863ba15bc
Yaremenko-R <roman.jaremenko@gmail.com>
2022-09-07 16:29:45 +0300

    L1_Ex4

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,19 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld() {
         System.out.println("Hello from Roman");
+    }
+
+    @Test
+    public void testGetText() {
+        Response response = RestAssured.
+                get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
