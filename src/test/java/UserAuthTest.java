c0210e1f771bc4f291c8b6adc164fc12d0945b2d
Yaremenko-R <roman.jaremenko@gmail.com>
2022-09-23 16:38:29 +0300

    testNegativeAuthUser added

--- src/test/java/UserAuthTest.java
+++ src/test/java/UserAuthTest.java
@@ -2,8 +2,10 @@ import io.restassured.RestAssured;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -48,5 +50,37 @@ public class UserAuthTest {
                 userIdOnCheck,
                 "user_id from auth request is not equal to user_id from check request"
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cookie", "headers"})
+    public void testNegativeAuthUser(String condition) {
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email","vinkotov@example.com");
+        authData.put("password","1234");
+
+        Response responseGetAuth = RestAssured
+                .given()
+                .body(authData)
+                .post("https://playground.learnqa.ru/api/user/login")
+                .andReturn();
+
+        Map<String, String> cookies = responseGetAuth.getCookies();
+        Headers headers = responseGetAuth.getHeaders();
+
+        RequestSpecification spec = RestAssured.given();
+        spec.baseUri("https://playground.learnqa.ru/api/user/auth");
+
+        if (condition.equals("cookie")) {
+            spec.cookie("auth_sid", cookies.get("auth_sid"));
+        } else if (condition.equals("headers")) {
+            spec.header("x-csrf-token", cookies.get("x-csrf-token"));
+        } else {
+            throw new IllegalArgumentException("Condition value is unknown: " + condition);
+        }
+
+        JsonPath responseForCheck = spec.get().jsonPath();
+        assertEquals(0,responseForCheck.getInt("user_id"),"user_id should be 0 for unauth request");
+        
     }
 }
