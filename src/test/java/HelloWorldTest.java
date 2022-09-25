cd73f47f697ccc8148186c7eab532d463aaecb4b
Yaremenko-R <roman.jaremenko@gmail.com>
2022-09-26 01:13:51 +0300

    L3_Ex13 testUserAgent added

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,6 +3,8 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -182,5 +184,68 @@ public class HelloWorldTest {
 
         String responseHeader = responseGetHeader.getHeader("x-secret-homework-header");
         assertEquals("Some secret value", responseHeader, "Wrong header");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"})
+    public void testUserAgent(String userAgent) {
+
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("User-Agent", userAgent);
+
+        JsonPath response = RestAssured
+                .given()
+                .headers(queryParams)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        System.out.println("-------------- User-Agent --------------");
+        String responsePlatform = response.getString("platform");
+        String responseBrowser = response.getString("browser");
+        String responseDevice = response.getString("device");
+        response.prettyPrint();
+        if (userAgent.contentEquals("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30")) {
+            String expectedPlatform = "Mobile";
+            String expectedBrowser = "No";
+            String expectedDevice = "Android";
+            assertEquals(expectedPlatform, responsePlatform, "Platform in response: " + responsePlatform + "doesn't match with expected: " + expectedPlatform);
+            assertEquals(expectedBrowser, responseBrowser, "Browser in response: " + responseBrowser + "doesn't match with expected: " + expectedBrowser);
+            assertEquals(expectedDevice, responseDevice, "Device in response: " + responseDevice + "doesn't match with expected: " + expectedDevice);
+        } else if (userAgent.contentEquals("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1")) {
+            String expectedPlatform = "Mobile";
+            String expectedBrowser = "Chrome";
+            String expectedDevice = "iOS";
+            assertEquals(expectedPlatform, responsePlatform, "Platform in response: " + responsePlatform + "doesn't match with expected: " + expectedPlatform);
+            assertEquals(expectedBrowser, responseBrowser, "Browser in response: " + responseBrowser + "doesn't match with expected: " + expectedBrowser);
+            assertEquals(expectedDevice, responseDevice, "Device in response: " + responseDevice + "doesn't match with expected: " + expectedDevice);
+        } else if (userAgent.contentEquals("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")) {
+            String expectedPlatform = "Googlebot";
+            String expectedBrowser = "Unknown";
+            String expectedDevice = "Unknown";
+            assertEquals(expectedPlatform, responsePlatform, "Platform in response: " + responsePlatform + "doesn't match with expected: " + expectedPlatform);
+            assertEquals(expectedBrowser, responseBrowser, "Browser in response: " + responseBrowser + "doesn't match with expected: " + expectedBrowser);
+            assertEquals(expectedDevice, responseDevice, "Device in response: " + responseDevice + "doesn't match with expected: " + expectedDevice);
+        } else if (userAgent.contentEquals("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0")) {
+            String expectedPlatform = "Web";
+            String expectedBrowser = "Chrome";
+            String expectedDevice = "No";
+            assertEquals(expectedPlatform, responsePlatform, "Platform in response: " + responsePlatform + "doesn't match with expected: " + expectedPlatform);
+            assertEquals(expectedBrowser, responseBrowser, "Browser in response: " + responseBrowser + "doesn't match with expected: " + expectedBrowser);
+            assertEquals(expectedDevice, responseDevice, "Device in response: " + responseDevice + "doesn't match with expected: " + expectedDevice);
+        } else if (userAgent.contentEquals("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1")) {
+            String expectedPlatform = "Mobile";
+            String expectedBrowser = "No";
+            String expectedDevice = "iPhone";
+            assertEquals(expectedPlatform, responsePlatform, "Platform in response: " + responsePlatform + "doesn't match with expected: " + expectedPlatform);
+            assertEquals(expectedBrowser, responseBrowser, "Browser in response: " + responseBrowser + "doesn't match with expected: " + expectedBrowser);
+            assertEquals(expectedDevice, responseDevice, "Device in response: " + responseDevice + "doesn't match with expected: " + expectedDevice);
+        } else {
+            throw new IllegalArgumentException("Unknown User-Agent: " + userAgent);
+        }
     }
 }
