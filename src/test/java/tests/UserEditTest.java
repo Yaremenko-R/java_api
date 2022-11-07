4a553bfb40f5e959465efed1ad4a2c30dd6f80f8
Yaremenko-R <roman.jaremenko@gmail.com>
2022-11-07 12:39:21 +0300

    L4_Ex17 testEditUserFirstName() added

--- src/test/java/tests/UserEditTest.java
+++ src/test/java/tests/UserEditTest.java
@@ -203,4 +203,48 @@ public class UserEditTest extends BaseTestCase {
         Assertions.assertResponseTextEquals(responseEditUser, "Invalid email format");
     }
 
+    @Test
+    public void testEditUserFirstName() {
+        //GENERATE USER
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        JsonPath responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+
+        //LOGIN
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", userData.get("email"));
+        authData.put("password", userData.get("password"));
+
+        Response responseGetAuth = apiCoreRequest
+                .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        //EDIT
+        String newName = "n";
+        Map<String, String> editData = new HashMap<>();
+        editData.put("firstName", newName);
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        Response responseEditUser = apiCoreRequest
+                .makePutRequestFlashRoal("https://playground.learnqa.ru/api/user/" + userId,
+                        header,
+                        cookie,
+                        editData);
+
+        //GET
+        Response responseUserData = apiCoreRequest
+                .makeGetRequest("https://playground.learnqa.ru/api/user/" + userId,
+                        header,
+                        cookie);
+
+        Assertions.assertResponseCodeEquals(responseEditUser, 400);
+        Assertions.assertResponseTextEquals(responseEditUser, "{\"error\":\"Too short value for field firstName\"}");
+    }
 }
