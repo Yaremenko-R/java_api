b562f199c8ce6396ca626f0f4ce9651492925aa8
Yaremenko-R <roman.jaremenko@gmail.com>
2022-11-07 13:54:20 +0300

    testDeleteUserWithAuth() added

--- src/test/java/tests/UserDeleteTest.java
+++ src/test/java/tests/UserDeleteTest.java
@@ -1,9 +1,12 @@
 package tests;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import lib.ApiCoreRequest;
 import lib.Assertions;
 import lib.BaseTestCase;
+import lib.DataGenerator;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -35,5 +38,45 @@ class UserDeleteTest extends BaseTestCase {
 
         Assertions.assertResponseTextEquals(responseDelete,
                 "Please, do not delete test users with ID 1, 2, 3, 4 or 5.");
+    }
+
+    @Test
+    void testDeleteUserWithAuth() {
+        //GENERATE USER
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+
+        JsonPath responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .jsonPath();
+
+        String userId = responseCreateAuth.getString("id");
+
+        //LOGIN
+        Map<String, String> authData = new HashMap<>();
+        authData.put("email", userData.get("email"));
+        authData.put("password", userData.get("password"));
+
+        Response responseGetAuth = apiCoreRequest
+                .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+        String header = this.getHeader(responseGetAuth, "x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+        //DELETE
+        Response responseDelete = apiCoreRequest
+                .makeDeleteRequestWithTokenAndCookie("https://playground.learnqa.ru/api/user/" + userId,
+                        header,
+                        cookie);
+
+        //GET
+        Response responseUserData = apiCoreRequest
+                .makeGetRequest("https://playground.learnqa.ru/api/user/" + userId,
+                        header,
+                        cookie);
+
+        Assertions.assertResponseCodeEquals(responseUserData, 404);
+        Assertions.assertResponseTextEquals(responseUserData, "User not found");
     }
 }
